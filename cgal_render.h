60f9786b0c68737ef4eb5931f8a4ebf6a92c00e4
Ferran Mesas <ferran.mesas01@gmail.com>
2014-03-12 23:09:10 +0100

    added include guards to cgal_render

--- cgal_render.h
+++ cgal_render.h
@@ -1,3 +1,6 @@
+#ifndef _cgal_render_H
+#define _cgal_render_H
+
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
@@ -11,3 +14,5 @@ typedef Polyhedron::Facet_const_iterator Facet_const_iterator;
 void cgal_render(const Polyhedron & p);
 
 void draw_facet(const Facet_const_iterator & f);
+
+#endif
